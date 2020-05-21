742b7b37526e61f1a3e708dd959e8a6d401b1ac1
sihan-tawsik <sihantawsik@gmail.com>
2020-05-21 12:45:23 +0600

    updated asyncio

--- src/views/recording/RecordFile.py
+++ src/views/recording/RecordFile.py
@@ -17,13 +17,13 @@ class RecordAPI(MethodView):
         try:
             file = request.files["audio"]
             filename = request.form.get("filename")
+            asyncio.run(firebase_post(file, filename))
             sentece_no = filename.split("-")[0]
             sentence = Sentence.query.filter_by(id=int(sentece_no)).first()
             sentence.samples += 1
             recording = Recording(filename, int(sentece_no))
             db.session.add(recording)
             db.session.commit()
-            firebase_post(file, filename)
         except Exception as e:
             raise e
         return make_response(jsonify(response_object)), 200
