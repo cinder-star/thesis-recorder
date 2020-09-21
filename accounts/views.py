a0f939b1cf58310b4a618e9de1a7ca83aba3db37
sihan <sihantawsik@gmail.com>
2020-09-21 18:57:43 +0600

    changed http status for already created user

--- accounts/views.py
+++ accounts/views.py
@@ -23,4 +23,4 @@ class UserList(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_409_CONFLICT)
